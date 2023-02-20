4d535a1f8dcdfe8915597b2b6ee531e0b48911a8
REETESH DESHMUKH <reeteshdeshmukhbpl@gmail.com>
2023-02-20 08:14:57 +0530

    intial commit

---
+++ 201IT248_A1.js
@@ -0,0 +1,34 @@
+var titles = []
+var curr = ""
+function signup(){
+    let email = document.getElementById('email1').value;
+    alert(`user with email : ${email} signed up!!`);
+    document.getElementById('user').value = email;
+}
+function signin(){
+    let email = document.getElementById('email2').value;
+    alert(`user with email : ${email} logged in!!`);   
+}
+function createBlog(){
+    let title = document.getElementById('title').value;
+    let subtitle = document.getElementById('subtitle').value;
+    let text = document.getElementById('blog').value;
+    titles.push(title);
+    alert(`Blog created! \ntitle : ${title}  subtitle : ${subtitle} \nBlogText : ${text}`);
+}
+
+function q(){
+    var email3 = document.getElementById('email3').value;
+    var query = document.getElementById('query').value;
+    alert(`user ${email3} asked query : ${query}`);
+}
+
+function findBlog(){
+    var title = document.getElementById('titblog').value;
+    var ans = 0;
+    for(let i=0;i<titles.length();i++){
+        if(titles[i] == title) ans = 1;
+    }
+    if(ans) alert(`there is one blog titled ${title}`);
+    else alert("sorry there is no such email!!");
+}
